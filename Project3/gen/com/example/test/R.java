70566612e8ff66d32d11b9e91e51aab52d8fdcfe
alejandrovega44 <alejandrovega44@gmail.com>
2013-11-08 16:43:12 -0600

    Launch button issue resolved
    must now click a species and area to enable launch button

--- Project3/gen/com/example/test/R.java
+++ Project3/gen/com/example/test/R.java
@@ -38,12 +38,14 @@ public final class R {
         public static final int textView2=0x7f080006;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int second_activity_display=0x7f030001;
+        public static final int activity_display_message=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int second_activity_display=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int second=0x7f070001;
+        public static final int display_message=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int second=0x7f070002;
     }
     public static final class string {
         public static final int Button=0x7f050003;
--- Project3/src/com/example/test/MainActivity.java
+++ Project3/src/com/example/test/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.test;
 
+import com.example.test.R.id;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +16,12 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		//Button launch = (Button) findViewById(R.id.button1);
+		//launch.setEnabled(true);
+		
+	
+		
+		
 	}
 
 	@Override
@@ -26,6 +36,39 @@ public class MainActivity extends Activity {
 		 Intent intent = new Intent();
 		 intent.setClass(this,SecondActivity.class);
 		 startActivity(intent);
+		 
+		
 	}  
 
-}
+	public void onRadioButtonClicked(View view)
+	{
+		Button launch = (Button) findViewById(R.id.button1);
+		RadioButton human, mouse, rat, brain, spine, eye;
+		
+		human = (RadioButton)findViewById(R.id.radioButton1);
+		mouse = (RadioButton)findViewById(R.id.radioButton2);
+		rat = (RadioButton)findViewById(R.id.radioButton3);
+		spine = (RadioButton)findViewById(R.id.radioButton4);
+		brain = (RadioButton)findViewById(R.id.radioButton5);
+		eye = (RadioButton)findViewById(R.id.radioButton6);
+		
+		if(human.isChecked() && brain.isChecked() || human.isChecked() && eye.isChecked() || human.isChecked() && spine.isChecked()) 
+		{
+			launch.setEnabled(true);
+		}
+		
+		if(rat.isChecked() && brain.isChecked() || rat.isChecked() && eye.isChecked() || rat.isChecked() && spine.isChecked()) 
+		{
+			launch.setEnabled(true);
+		}
+		
+		if(mouse.isChecked() && brain.isChecked() || mouse.isChecked() && eye.isChecked() || mouse.isChecked() && spine.isChecked()) 
+		{
+			launch.setEnabled(true);
+		}
+		
+	}
+	
+	
+}	
+
