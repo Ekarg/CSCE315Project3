e88934cd1b9618a339f60b2db895954fe4ed8bdf
guardado18 <torviolet18@gmail.com>
2013-11-13 17:55:54 -0600

    OPENGL LINES
    Got a color full line here is the open gl file that we worked on today
    now to find a way to add the mouse pad

--- OpenGLTurtorial/src/com/example/project3/LessonOneRenderer.java
+++ OpenGLTurtorial/src/com/example/project3/LessonOneRenderer.java
@@ -38,8 +38,7 @@ public class LessonOneRenderer implements GLSurfaceView.Renderer
         
         /** Store our model data in a float buffer. */
         private final FloatBuffer mTriangle1Vertices;
-        private final FloatBuffer mTriangle2Vertices;
-        private final FloatBuffer mTriangle3Vertices;
+
 
         /** This will be used to pass in the transformation matrix. */
         private int mMVPMatrixHandle;
@@ -66,7 +65,9 @@ public class LessonOneRenderer implements GLSurfaceView.Renderer
         private final int mColorOffset = 3;
         
         /** Size of the color data in elements. */
-        private final int mColorDataSize = 4;                
+        private final int mColorDataSize = 4;
+
+		public float mAngle;                
                                 
         /**
          * Initialize the model data.
@@ -83,48 +84,17 @@ public class LessonOneRenderer implements GLSurfaceView.Renderer
                     1.0f, 0.0f, 0.0f, 1.0f,
                     
                     0.5f, -0.25f, 0.0f,
-                    0.0f, 0.0f, 1.0f, 1.0f,
-                    
-                    0.0f, 0.559016994f, 0.0f, 
-                    0.0f, 1.0f, 0.0f, 1.0f};
+                    0.0f, 0.0f, 1.0f, 1.0f};
                 
-                // This triangle is yellow, cyan, and magenta.
-                final float[] triangle2VerticesData = {
-                                // X, Y, Z, 
-                                // R, G, B, A
-                    -0.5f, -0.25f, 0.0f, 
-                    1.0f, 1.0f, 0.0f, 1.0f,
-                    
-                    0.5f, -0.25f, 0.0f, 
-                    0.0f, 1.0f, 1.0f, 1.0f,
-                    
-                    0.0f, 0.559016994f, 0.0f, 
-                    1.0f, 0.0f, 1.0f, 1.0f};
-                
-                // This triangle is white, gray, and black.
-                final float[] triangle3VerticesData = {
-                                // X, Y, Z, 
-                                // R, G, B, A
-                    -0.5f, -0.25f, 0.0f, 
-                    1.0f, 1.0f, 1.0f, 1.0f,
-                    
-                    0.5f, -0.25f, 0.0f, 
-                    0.5f, 0.5f, 0.5f, 1.0f,
-                    
-                    0.0f, 0.559016994f, 0.0f, 
-                    0.0f, 0.0f, 0.0f, 1.0f};
+
+
                 
                 // Initialize the buffers.
                 mTriangle1Vertices = ByteBuffer.allocateDirect(triangle1VerticesData.length * mBytesPerFloat)
         .order(ByteOrder.nativeOrder()).asFloatBuffer();
-                mTriangle2Vertices = ByteBuffer.allocateDirect(triangle2VerticesData.length * mBytesPerFloat)
-        .order(ByteOrder.nativeOrder()).asFloatBuffer();
-                mTriangle3Vertices = ByteBuffer.allocateDirect(triangle3VerticesData.length * mBytesPerFloat)
-        .order(ByteOrder.nativeOrder()).asFloatBuffer();
-                                        
+
                 mTriangle1Vertices.put(triangle1VerticesData).position(0);
-                mTriangle2Vertices.put(triangle2VerticesData).position(0);
-                mTriangle3Vertices.put(triangle3VerticesData).position(0);
+   
         }
         
         @Override
@@ -309,22 +279,10 @@ public class LessonOneRenderer implements GLSurfaceView.Renderer
         
         // Draw the triangle facing straight on.
         Matrix.setIdentityM(mModelMatrix, 0);
-        Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);        
+       // Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);        
         drawTriangle(mTriangle1Vertices);
         
-        // Draw one translated a bit down and rotated to be flat on the ground.
-        Matrix.setIdentityM(mModelMatrix, 0);
-        Matrix.translateM(mModelMatrix, 0, 0.0f, -1.0f, 0.0f);
-        Matrix.rotateM(mModelMatrix, 0, 90.0f, 1.0f, 0.0f, 0.0f);
-        Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);        
-        drawTriangle(mTriangle2Vertices);
-        
-        // Draw one translated a bit to the right and rotated to be facing to the left.
-        Matrix.setIdentityM(mModelMatrix, 0);
-        Matrix.translateM(mModelMatrix, 0, 1.0f, 0.0f, 0.0f);
-        Matrix.rotateM(mModelMatrix, 0, 90.0f, 0.0f, 1.0f, 0.0f);
-        Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);
-        drawTriangle(mTriangle3Vertices);
+
         }        
         
         /**
@@ -357,6 +315,6 @@ public class LessonOneRenderer implements GLSurfaceView.Renderer
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
 
         GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mMVPMatrix, 0);
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);                               
+        GLES20.glDrawArrays(GLES20.GL_LINES, 0, 3);                               
         }
 }
