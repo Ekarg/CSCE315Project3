99521e7f06f166e996a12526f8eda133b789e972
alejandrovega44 <alejandrovega44@gmail.com>
2013-11-15 16:29:20 -0600

    Revert "Merge of XML Reader"
    This reverts commit 7a50250a042346b974e7225332884606f5c16bb1.

--- Project3/gen/com/example/test/R.java
+++ Project3/gen/com/example/test/R.java
@@ -17,54 +17,50 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000d;
-        public static final int button1=0x7f090000;
-        public static final int radioButton1=0x7f090003;
-        public static final int radioButton2=0x7f090004;
-        public static final int radioButton3=0x7f090005;
-        public static final int radioButton4=0x7f090009;
-        public static final int radioButton5=0x7f090008;
-        public static final int radioButton6=0x7f09000a;
-        public static final int radioGroup1=0x7f090002;
-        public static final int radioGroup2=0x7f090007;
-        public static final int textTitle1=0x7f09000b;
-        public static final int textTitle2=0x7f09000c;
-        public static final int textView1=0x7f090001;
-        public static final int textView2=0x7f090006;
+        public static final int action_settings=0x7f08000d;
+        public static final int button1=0x7f080000;
+        public static final int radioButton1=0x7f080003;
+        public static final int radioButton2=0x7f080004;
+        public static final int radioButton3=0x7f080005;
+        public static final int radioButton4=0x7f080009;
+        public static final int radioButton5=0x7f080008;
+        public static final int radioButton6=0x7f08000a;
+        public static final int radioGroup1=0x7f080002;
+        public static final int radioGroup2=0x7f080007;
+        public static final int textTitle1=0x7f08000b;
+        public static final int textTitle2=0x7f08000c;
+        public static final int textView1=0x7f080001;
+        public static final int textView2=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int second_activity_display=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int second=0x7f080001;
-    }
-    public static final class raw {
-        public static final int data_large=0x7f040000;
-        public static final int data_small=0x7f040001;
+        public static final int main=0x7f070000;
+        public static final int second=0x7f070001;
     }
     public static final class string {
-        public static final int Button=0x7f060003;
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int area=0x7f060009;
-        public static final int brain=0x7f06000b;
-        public static final int eye=0x7f06000a;
-        public static final int hello_world=0x7f060002;
-        public static final int human=0x7f060006;
-        public static final int mouse=0x7f060007;
-        public static final int rat=0x7f060008;
-        public static final int species=0x7f060005;
-        public static final int spine=0x7f06000c;
-        public static final int title_activity_second=0x7f060004;
+        public static final int Button=0x7f050003;
+        public static final int action_settings=0x7f050001;
+        public static final int app_name=0x7f050000;
+        public static final int area=0x7f050009;
+        public static final int brain=0x7f05000b;
+        public static final int eye=0x7f05000a;
+        public static final int hello_world=0x7f050002;
+        public static final int human=0x7f050006;
+        public static final int mouse=0x7f050007;
+        public static final int rat=0x7f050008;
+        public static final int species=0x7f050005;
+        public static final int spine=0x7f05000c;
+        public static final int title_activity_second=0x7f050004;
     }
     public static final class style {
         /** 
@@ -88,10 +84,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f060001;
     }
 }
